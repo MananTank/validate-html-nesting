fe8e24df67d0f63a0424db30fea1a6da32fe3bcd
Manan Tank <manantankm@gmail.com>
2023-01-11 12:55:25 +0530

    Update comments

--- src/index.js
+++ src/index.js
@@ -7,38 +7,38 @@ const {
 } = require('./mapping');
 
 /**
- * returns true if given parent-child nesting is valid html
+ * returns true if given parent-child nesting is valid HTML
  * @param {string} child
  * @param {string} parent
  * @returns {boolean}
  */
 function isValidHTMLNesting(parent, child) {
-	// if we know the list of children that are the only valid chidren for given parent
+	// if we know the list of children that are the only valid children for the given parent
 	if (parent in onlyValidChildren) {
 		return onlyValidChildren[parent].has(child);
 	}
 
-	// if we know the list of parents that are the only valid parents for given child
+	// if we know the list of parents that are the only valid parents for the given child
 	if (child in onlyValidParents) {
 		return onlyValidParents[child].has(parent);
 	}
 
-	// if we know the lsit of children that not valid for given parent
+	// if we know the list of children that are NOT valid for the given parent
 	if (parent in knownInvalidChildren) {
-		// check if child is in the list of invalid children
+		// check if the child is in the list of invalid children
 		// if so, return false
 		if (knownInvalidChildren[parent].has(child)) return false;
 	}
 
-	// if we know list of parents that are not valid for given children
+	// if we know the list of parents that are NOT valid for the given child
 	if (child in knownInvalidParents) {
-		// check if parent is in the list of invalid parents
+		// check if the parent is in the list of invalid parents
 		// if so, return false
 		if (knownInvalidParents[child].has(parent)) return false;
 	}
 
-	// svg tags should only contain svg tags
-	if (svgTags.has(parent) && child !== "a" && !svgTags.has(child)) {
+	// SVG tags should only contain SVG tags or anchor tag
+	if (svgTags.has(parent) && child !== 'a' && !svgTags.has(child)) {
 		return false;
 	}
 
